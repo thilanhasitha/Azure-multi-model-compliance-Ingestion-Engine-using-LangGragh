8147ccbae59dd568633f84cfd57419b05deb8b25
Thilan Hasitha <156594121+thilanhasitha@users.noreply.github.com>
2026-02-20 12:58:53 +0530

    folder structure created

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Azure-multi-model-compliance-Ingestion-Engine-using-LangGragh
+![alt text](Project2_Langgraph_Architecture.png)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from azure-multi-model-compliance-ingestion-engine-using-langgragh!")
+
+
+if __name__ == "__main__":
+    main()
